447c9518414ad57590498d79e3b894b1bea3693f
Icaro Assis <icaroassisfreitas@gmail.com>
2022-01-26 14:38:54 -0300

    cria componente de oportunidades de trabalhos

---
+++ classe-01/devmatch/src/components/JobOpportunity/index.js
@@ -0,0 +1,50 @@
+import "./styles.css";
+//import GoogleLogo from "../../assets/google-logo.svg";
+
+function JobOpportunity({
+  img_url,
+  name,
+  description,
+  requirements,
+  seniority,
+  salary,
+  benefits,
+}) {
+  return (
+    <div className="container-job-opportunity">
+      <img src={img_url} alt="logo" className="logo" />
+      <h2>{name}</h2>
+      <div className="details">
+        <strong>Descrição</strong>
+        <p>{description}</p>
+      </div>
+      <div className="details">
+        <strong>Requisitos</strong>
+        <div className="flex-row">
+          {requirements.map((requirement) => (
+            <div className="badge-blue" key={requirement}>
+              {requirement}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="details">
+        <strong>Senioridade</strong>
+        <span>{seniority}</span>
+      </div>
+      <div className="details">
+        <strong>Salário / Benefícios</strong>
+        <span>
+          R$ {salary} /{" "}
+          {benefits.map((item, index) => (
+            <span>
+              {item} {index + 1 < benefits.length && ", "}
+            </span>
+          ))}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default JobOpportunity;
